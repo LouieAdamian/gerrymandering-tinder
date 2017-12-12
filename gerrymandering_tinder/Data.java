f6f1c0120e1e03ac77db9c43f46006bcf366a933
Louie Adamian <louieadamian@gmail.com>
2017-12-12 11:29:27 -0500

    Merge branch 'master' into Chris's-branch

--- gerrymandering_tinder/Data.java
+++ gerrymandering_tinder/Data.java
@@ -7,8 +7,15 @@ public class Data{
 
 
 
-
-  void addDisctict(String name,String photo, HashMap conversation){
-      Data.districts.add(new District(name, false, photo, conversation));
+  public static void addDisctict(String name,String photo, final String answer0, final String answer1, final String answer2){
+    Data.districts.add(new District(name, false, photo, new HashMap<String,String>(){
+                        {
+                                put(questions[0], answer0);
+                                put(questions[1], answer1);
+                                put(questions[2], answer2);
+                        };
+                      }
+                      )
+                      );
   }
 }
---
+++ gerrymandering_tinder/chat_test/chat_test.pde
@@ -0,0 +1,12 @@
+void setup(){
+
+}
+void draw(){
+
+}
+
+void chat(class District) {
+text(District.name);
+PImage profile = loadImage(District.name);
+
+}
