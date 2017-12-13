355b77a5a85138ae49d64eded3dc6e827e460127
Louie Adamian <louieadamian@gmail.com>
2017-12-13 09:22:17 -0500

    Merge remote-tracking branch 'origin/Chris's-branch' into Chris's-branch

--- gerrymandering_tinder/Data.java
+++ gerrymandering_tinder/Data.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+<<<<<<< HEAD
+public class Data {
+public static ArrayList<Object> districts = new ArrayList<Object>();
+public static String[] questions = {"That's the longest district I've ever seen.", "I like that disenfranchisement on you.", "TODO"};
+=======
 public class Data{
-  public static String[] questions ={"","",""};
+  String[] questions ={"",","};
   public static ArrayList<Object> districts = new ArrayList<Object>();
+>>>>>>> parent of ee3835a... Merge branch 'Chris's-branch' of https://github.com/LouieAdamian/gerrymandering-tinder into Chris's-branch
 
 
 
